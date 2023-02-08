f70165ecc664c370b5193a3ff38da9c1d9b8acb6
Yixue Zhao <felicitia2010@gmail.com>
2023-02-08 15:52:35 -0500

    Create build_criu.sh

---
+++ cross-compile/build_criu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "running script: build_criu.sh"
+
+. ./config.sh
+
+cd $CRIU_ROOT_DIR
+
+export PATH=$BUILD_ROOT_DIR/x86_64_pb_install/bin:$PATH
+export PKG_CONFIG_PATH=$BUILD_ROOT_DIR/arm64_pb_install/lib/pkgconfig:$PKG_CONFIG_PATH
+
+make mrproper
+
+file="/home/yixue/criu/compel/plugins/include/uapi/std/syscall-aux.S"
+if [ -f "$file" ] ; then
+    rm "$file"
+fi
+
+file="/home/yixue/criu/compel/plugins/include/uapi/std/syscall-aux.h"
+if [ -f "$file" ] ; then
+    rm "$file"
+fi
+
+LDFLAGS=$(pkg-config --libs libprotobuf-c)
+LDFLAGS+=" -rpath /usr/aarch64-linux-gnu/lib"
+
+# echo $LDFLAGS
+# read tmp
+
+V=1 \
+ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- \
+CFLAGS=$(pkg-config --cflags libprotobuf-c) \
+LDFLAGS=$LDFLAGS \
+make -f Makefile-CC
+
