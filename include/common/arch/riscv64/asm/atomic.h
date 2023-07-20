e2fe7d76720cbf34647c045f5c095278e797a6da
Yixue Zhao <felicitia2010@gmail.com>
2023-07-20 12:25:38 -0400

    add riscv64 support to include/common/arch
    Signed-off-by: Yixue Zhao <felicitia2010@gmail.com>
    Co-Authored-By: Haorong Lu <ancientmodern4@gmail.com>
    Co-Authored-By: Haorong Lu <46423554+ancientmodern@users.noreply.github.com>

--- include/common/arch/riscv64/asm/atomic.h
+++ include/common/arch/riscv64/asm/atomic.h
@@ -5,10 +5,39 @@ typedef struct {
 	int counter;
 } atomic_t;
 
+/* Copied from the Linux header arch/riscv/include/asm/barrier.h */
 
-#define smp_mb() asm volatile("fence rw,rw" : : : "memory")
+#define nop()		__asm__ __volatile__ ("nop")
 
-/* Copied from the Linux kernel header arch/riscv64/include/asm/atomic.h */
+#define RISCV_FENCE(p, s) \
+	__asm__ __volatile__ ("fence " #p "," #s : : : "memory")
+
+/* These barriers need to enforce ordering on both devices or memory. */
+#define mb()		RISCV_FENCE(iorw,iorw)
+#define rmb()		RISCV_FENCE(ir,ir)
+#define wmb()		RISCV_FENCE(ow,ow)
+
+/* These barriers do not need to enforce ordering on devices, just memory. */
+#define __smp_mb()	RISCV_FENCE(rw,rw)
+#define __smp_rmb()	RISCV_FENCE(r,r)
+#define __smp_wmb()	RISCV_FENCE(w,w)
+
+#define __smp_store_release(p, v)					\
+do {									\
+	compiletime_assert_atomic_type(*p);				\
+	RISCV_FENCE(rw,w);						\
+	WRITE_ONCE(*p, v);						\
+} while (0)
+
+#define __smp_load_acquire(p)						\
+({									\
+	typeof(*p) ___p1 = READ_ONCE(*p);				\
+	compiletime_assert_atomic_type(*p);				\
+	RISCV_FENCE(r,rw);						\
+	___p1;								\
+})
+
+/* Copied from the Linux kernel header arch/riscv/include/asm/atomic.h */
 
 static inline int atomic_read(const atomic_t *v)
 {
@@ -20,42 +49,24 @@ static inline void atomic_set(atomic_t *v, int i)
 	v->counter = i;
 }
 
-// #define atomic_get atomic_read
+#define atomic_get atomic_read
 
 static inline int atomic_add_return(int i, atomic_t *v)
 {
-	unsigned long tmp;
 	int result;
 
-	asm volatile(
-		"1: lr.w %0, %2\n"
-		" add %0, %0, %3\n"
-		" sc.w %1, %0, %2\n"
-		" bnez %1, 1b"
-		: "=&r"(result), "=&r"(tmp), "+A"(v->counter)
-		: "Ir"(i)
-		: "memory");
-
-	smp_mb();
-	return result;
+        asm volatile(
+                "amoadd.w.aqrl %1, %2, %0"
+                : "+A" (v->counter), "=r" (result)
+                : "r" (i)
+                : "memory");
+	__smp_mb();
+        return result + i;
 }
 
 static inline int atomic_sub_return(int i, atomic_t *v)
 {
-	unsigned long tmp;
-	int result;
-
-	asm volatile(
-		"1: lr.w %0, %2\n"
-		" sub %0, %0, %3\n"
-		" sc.w %1, %0, %2\n"
-		" bnez %1, 1b"
-		: "=&r"(result), "=&r"(tmp), "+A"(v->counter)
-		: "Ir"(i)
-		: "memory");
-
-	smp_mb();
-	return result;
+	return atomic_add_return(-i, v);
 }
 
 static inline int atomic_inc(atomic_t *v)
@@ -73,32 +84,31 @@ static inline int atomic_dec(atomic_t *v)
 	return atomic_sub_return(1, v) + 1;
 }
 
-//  atomically compare and exchange a value in a memory location
-static inline int atomic_cmpxchg(atomic_t *ptr, int old, int new)
-{
-	unsigned long tmp;
-	int oldval;
-
-	smp_mb();
-
-	asm volatile(
-		"1: lr.w %0, %2\n"
-		" bne %0, %3, 2f\n"
-		" sc.w %1, %4, %2\n"
-		" bnez %1, 1b\n"
-		"2:"
-		: "=&r"(oldval), "=&r"(tmp), "+A"(ptr->counter)
-		: "r"(old), "r"(new)
-		: "memory");
-
-	smp_mb();
-	return oldval;
-}
-
 /* true if the result is 0, or false for all other cases. */
 #define atomic_dec_and_test(v) (atomic_sub_return(1, v) == 0)
 #define atomic_dec_return(v)   (atomic_sub_return(1, v))
 
 #define atomic_inc_return(v) (atomic_add_return(1, v))
+
+static inline int atomic_cmpxchg(atomic_t *ptr, int old, int new)
+{
+	unsigned long tmp;
+	int oldval;
+
+	__smp_mb();
+
+	asm volatile("1:\n"
+		     "  lr.w %1, %2\n"
+		     "  bne %1, %3, 2f\n"
+		     "  sc.w %0, %4, %2\n"
+		     "  bnez %0, 1b\n"
+		     "2:"
+		     : "=&r"(tmp), "=&r"(oldval), "+A"(ptr->counter)
+		     : "r"(old), "r"(new)
+		     : "memory");
+
+	__smp_mb();
+	return oldval;
+}
 
 #endif /* __CR_ATOMIC_H__ */
